f5c874092bf7b7ad008cb1b9e3905e76cb9ba6da
AlvinRHD <ezequielhernandes907@gmail.com>
2024-04-06 15:18:17 -0600

    SP

--- Insercion y sp.sql
+++ Insercion y sp.sql
@@ -80,7 +80,7 @@ exec dbo.sp_Clientes_Insert 'leo', 'Lolo', '76013988', 'estevensalmeron@gmail.co
 
 select * from Clientes
 
-
+------------------------------------------
 ------------------------------------------
 --Prodecimiento alamcenado: Actualiza los datos a la tabla Clientes
 -------------------------------------------
@@ -210,6 +210,133 @@ end;
 exec dbo.sp_Ventas_Delete 11
 
 select * from Ventas
+
+------------------------------------------
+--Prodecimiento alamcenado de la tabla Detalle_Venta
+-------------------------------------------
+
+CREATE PROCEDURE dbo.sp_DetallesVenta_GetAll
+AS
+BEGIN
+    SELECT Detalles_Venta.ID_Detalle, Detalles_Venta.ID_Venta, Detalles_Venta.ID_Producto, Detalles_Venta.Cantidad, Detalles_Venta.Precio_Unitario, Detalles_Venta.Subtotal, Detalles_Venta.Impuesto
+    FROM Detalles_Venta;
+END;
+
+INSERT INTO Clientes (Nombre, Direccion, Telefono, CorreoElectronico) 
+VALUES ('Cliente A', 'Calle Uno 456', '111222333', 'clienteA@example.com');
+
+INSERT INTO Ventas (ID_Cliente, Fecha, Total, EstadoVenta) 
+VALUES (8, '2024-04-01', 1600.00, 'Completada');
+
+INSERT INTO Detalles_Venta (ID_Venta, ID_Producto, Cantidad, Precio_Unitario, Subtotal, Impuesto) 
+VALUES (12, 1, 1, 1500.00, 1500.00, 150.00);
+
+INSERT INTO Productos (Nombre, Descripcion, Precio, Stock, ID_Categoria, ID_Proveedor) 
+VALUES ('Laptop', 'Laptop de última generación', 1500.00, 20, 7, 7);
+
+
+INSERT INTO Detalles_Venta (ID_Venta, ID_Producto, Cantidad, Precio_Unitario, Subtotal, Impuesto) 
+VALUES (12, 11, 1, 1500.00, 1500.00, 150.00);
+
+select * from Productos
+
+select * from Proveedores
+
+select * from Categorias
+
+select * from Clientes
+
+select * from Ventas
+
+select * from Detalles_Venta
+
+EXEC dbo.sp_DetallesVenta_GetAll 
+
+------------------------------------------
+--Prodecimiento alamcenado: Filtra los registros por el ID_Detalle_Venta
+-------------------------------------------
+ALTER PROCEDURE dbo.sp_DetallesVenta_GetByVentaID (@ID_Detalle INT)
+AS
+BEGIN
+    SELECT ID_Detalle, ID_Venta, ID_Producto, Cantidad, Precio_Unitario, Subtotal, Impuesto
+    FROM Detalles_Venta
+    WHERE ID_Detalle = @ID_Detalle;
+END;
+
+EXEC dbo.sp_DetallesVenta_GetByVentaID 3
+
+
+------------------------------------------
+--Prodecimiento alamcenado: Inserta datos a la tabla Detalle_Venta
+-------------------------------------------
+
+CREATE PROCEDURE dbo.sp_DetallesVenta_Insert 
+(
+    @ID_Venta INT,
+    @ID_Producto INT,
+    @Cantidad INT,
+    @Precio_Unitario DECIMAL(10, 2),
+    @Subtotal MONEY,
+    @Impuesto MONEY
+)
+AS
+BEGIN
+    INSERT INTO Detalles_Venta (ID_Venta, ID_Producto, Cantidad, Precio_Unitario, Subtotal, Impuesto)
+    VALUES (@ID_Venta, @ID_Producto, @Cantidad, @Precio_Unitario, @Subtotal, @Impuesto);
+END;
+
+SELECT * FROM Detalles_Venta
+
+EXEC dbo.sp_DetallesVenta_Insert 12, 11, 3, 1000.00, 2000.00, 10.00
+
+
+
+------------------------------------------
+--Prodecimiento alamcenado: Actualiza los datos a la tabla Detalles_Ventas
+-------------------------------------------
+CREATE PROCEDURE dbo.sp_DetallesVenta_Update 
+(
+    @ID_Detalle INT,
+    @ID_Venta INT,
+    @ID_Producto INT,
+    @Cantidad INT,
+    @Precio_Unitario DECIMAL(10, 2),
+    @Subtotal MONEY,
+    @Impuesto MONEY
+)
+AS
+BEGIN
+    UPDATE Detalles_Venta
+    SET ID_Venta = @ID_Venta, ID_Producto = @ID_Producto, Cantidad = @Cantidad, Precio_Unitario = @Precio_Unitario, Subtotal = @Subtotal, Impuesto = @Impuesto
+    WHERE ID_Detalle = @ID_Detalle;
+END;
+
+
+EXEC dbo.sp_DetallesVenta_Update 3, 12, 11, 3, 1000.00, 4000.00, 20.00
+
+
+
+------------------------------------------
+--Prodecimiento alamcenado: Elimina los datos de la tabla Detalle_Venta
+-------------------------------------------
+CREATE PROCEDURE dbo.sp_DetallesVenta_Delete
+(
+    @ID_Detalle INT
+)
+AS
+BEGIN
+    DELETE FROM Detalles_Venta
+    WHERE ID_Detalle = @ID_Detalle;
+END;
+
+
+SELECT * FROM Detalles_Venta
+
+
+EXEC dbo.sp_DetallesVenta_Delete 3
+
+
+
 
 
 ------------------------------------
